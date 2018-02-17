00b2a82acce784d9fd3e71c62a9f1488f7d1784d
unixpunk <steve@hackode.com>
2018-02-17 17:26:09 -0600

    first commit

---
+++ tools/bin/dump1090.sh
@@ -0,0 +1,2 @@
+cd /dump1090
+./dump1090 --net --net-http-port 18080 --gain 70 --aggressive
---
+++ tools/bin/openwebrx.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/steve/Desktop/SDR/Pluto
+ln -s / /home/steve/Desktop/SDR/Pluto/staging
+cd /openwebrx
+python openwebrx.py
