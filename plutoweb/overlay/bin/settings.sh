3324878d71adfec0651f018424053f3375b7e3ec
unixpunk <steve@hackode.com>
2018-03-27 00:15:45 -0500

    change auto-update to stop the running app just in case

--- plutoweb/overlay/bin/settings.sh
+++ plutoweb/overlay/bin/settings.sh
@@ -55,7 +55,9 @@ case $i in
 	sed -i "s/autoupdate=$autoupdate/autoupdate=$2/" /root/temp-settings
 	autoupdate=$2
 	if [ "$2" = "y" ] || [ "$2" = "Y" ]; then
+		/etc/init.d/S95autostart stop
 		/bin/sh /etc/init.d/S97autoupdate restart
+		/etc/init.d/S95autostart start
 	fi
 	if [ "$2" = "n" ] || [ "$2" = "N" ]; then
 		/bin/sh /etc/init.d/S97autoupdate stop
