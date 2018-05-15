b339bb4c55e1551242e6b0d5757ecc81a161b444
unixpunk <steve@hackode.com>
2018-05-14 23:56:13 -0500

    Write optimization for nvram and fixes

--- plutoweb/overlay/bin/savenow.sh
+++ plutoweb/overlay/bin/savenow.sh
@@ -1,5 +1,57 @@
 #!/bin/sh
 . /bin/readsettings.sh
+flash_indication_on() {
+        echo timer > /sys/class/leds/led0:green/trigger
+        echo 40 > /sys/class/leds/led0:green/delay_off
+        echo 40 > /sys/class/leds/led0:green/delay_on
+}
+
+flash_indication_off() {
+        echo heartbeat > /sys/class/leds/led0:green/trigger
+}
+
+write_all() {
+        fw_printenv qspiboot
+        if [ $? -eq 0 ]; then
+                flash_indication_on
+                echo "Wait while writing to NVRAM..."
+                echo "autostart $autostart" >/opt/fw_set.tmp
+                echo "autoreboot $autoreboot" >> /opt/fw_set.tmp
+                echo "autoupdate $autoupdate" >> /opt/fw_set.tmp
+                echo "center_freq $center_freq" >> /opt/fw_set.tmp
+                echo "start_freq $start_freq" >> /opt/fw_set.tmp
+                echo "samp_rate $samp_rate" >> /opt/fw_set.tmp
+                echo "start_mod $start_mod" >> /opt/fw_set.tmp
+                echo "rf_gain $rf_gain" >> /opt/fw_set.tmp
+                echo "ppm $ppm" >> /opt/fw_set.tmp
+                fw_setenv -s /opt/fw_set.tmp
+                rm /opt/fw_set.tmp
+                echo "Done."
+                flash_indication_off
+        fi
+}
+
+erase_all() {
+        fw_printenv qspiboot
+        if [ $? -eq 0 ]; then
+                flash_indication_on
+                echo "Wait while erasing settings from NVRAM..."
+                echo autostart >/opt/fw_set.tmp
+                echo autoreboot >>/opt/fw_set.tmp
+                echo autoupdate >>/opt/fw_set.tmp
+                echo center_freq >>/opt/fw_set.tmp
+                echo start_freq >>/opt/fw_set.tmp
+                echo samp_rate >>/opt/fw_set.tmp
+                echo start_mod >>/opt/fw_set.tmp
+                echo rf_gain >>/opt/fw_set.tmp
+                echo ppm >>/opt/fw_set.tmp
+                fw_setenv -s /opt/fw_set.tmp
+                rm /opt/fw_set.tmp
+                echo "Done."
+                flash_indication_off
+        fi
+}
+
 echo "Changes saved here will take effect now and persist across reboots."
 echo "If you (re)flash the PlutoSDR you will lose your settings."
 if [ "$1" = "yes" ]; then
@@ -21,55 +73,3 @@ case $yn2 in
 	erase_all
 	exit;;
 esac
-
-write_all() {
-	fw_printenv qspiboot
-	if [ $? -eq 0 ]; then
-		flash_indication_on
-		echo "Wait while writing to NVRAM..."
-		echo "autostart $autostart" >/opt/fw_set.tmp
-		echo "autoreboot $autoreboot" >> /opt/fw_set.tmp
-		echo "autoupdate $autoupdate" >> /opt/fw_set.tmp
-		echo "center_freq $center_freq" >> /opt/fw_set.tmp
-		echo "start_freq $start_freq" >> /opt/fw_set.tmp
-		echo "samp_rate $samp_rate" >> /opt/fw_set.tmp
-		echo "start_mod $start_mod" >> /opt/fw_set.tmp
-		echo "rf_gain $rf_gain" >> /opt/fw_set.tmp
-		echo "ppm $ppm" >> /opt/fw_set.tmp
-		fw_setenv -s /opt/fw_set.tmp
-		rm /opt/fw_set.tmp
-		echo "Done."
-		flash_indication_off
-	fi
-}
-
-erase_all() {
-	fw_printenv qspiboot
-	if [ $? -eq 0 ]; then
-		flash_indication_on
-		echo "Wait while erasing settings from NVRAM..."
-		echo autostart >/opt/fw_set.tmp
-		echo autoreboot >>/opt/fw_set.tmp
-		echo autoupdate >>/opt/fw_set.tmp
-		echo center_freq >>/opt/fw_set.tmp
-		echo start_freq >>/opt/fw_set.tmp
-		echo samp_rate >>/opt/fw_set.tmp
-		echo start_mod >>/opt/fw_set.tmp
-		echo rf_gain >>/opt/fw_set.tmp
-		echo ppm >>/opt/fw_set.tmp
-		fw_setenv -s /opt/fw_set.tmp
-		rm /opt/fw_set.tmp
-		echo "Done."
-		flash_indication_off
-	fi	
-}
-
-flash_indication_on() {
-        echo timer > /sys/class/leds/led0:green/trigger
-        echo 40 > /sys/class/leds/led0:green/delay_off
-        echo 40 > /sys/class/leds/led0:green/delay_on
-}
-
-flash_indication_off() {
-        echo heartbeat > /sys/class/leds/led0:green/trigger
-}
