944ec56ecfbf4a390b3abb6083183c3b35f5e898
unixpunk <steve@hackode.com>
2018-08-14 11:54:41 -0500

    Added auto-restart of SoapySDRServer if it crashes

--- plutoweb/overlay/bin/soapyremote.sh
+++ plutoweb/overlay/bin/soapyremote.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+## Pull in settings
+. /bin/readsettings.sh >/dev/null
+
 ## Start SoapySDRServer
 cd
+while [[ "$autostart" = "SoapyRemote" ]]; do
 /bin/SoapySDRServer --bind
+sleep 1
+. /bin/readsettings.sh >/dev/null
+done
