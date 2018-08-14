fcfda351c64dab288be342baa4d9bb0570aa9f94
unixpunk <steve@hackode.com>
2018-08-14 09:40:05 -0500

    Fixed issue with SoapyRemote path The workaround was in the wrong file.  This will eventually go away the next time SoapySDR is built, I should be able to use the new cmake options to avoid the need for this.

--- plutoweb/overlay/bin/soapyremote.sh
+++ plutoweb/overlay/bin/soapyremote.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+## This is a workaround for an issue where SoapySDR remembers its install path
+## and tries to use it as its 'root' directory...Fake it.
+if [ ! -d "/home/steve/Desktop/SDR/Pluto" ]; then
+        mkdir -p /home/steve/Desktop/SDR/Pluto
+        ln -s / /home/steve/Desktop/SDR/Pluto/staging
+fi
+
+## Start SoapySDRServer
 cd
 /bin/SoapySDRServer --bind
