dd324835675e4e41d5b8cbd67adcf375921b2808
unixpunk <steve@hackode.com>
2019-03-14 01:59:09 -0500

    Cleanup

--- plutoweb/overlay/bin/dump1090.sh
+++ plutoweb/overlay/bin/dump1090.sh
@@ -2,10 +2,5 @@
 ## Pull in settings
 . /bin/readsettings.sh >/dev/null
 
-## Run dump1090 with the best options available in this fork
 cd /dump1090
-while [[ "$autostart" = "dump1090" ]]; do
 ./dump1090 --net --net-http-port 8080 --gain 89 --aggressive
-sleep 1
-. /bin/readsettings.sh >/dev/null
-done
