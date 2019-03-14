7a5804fe38356f064a56dc2ab23519791ea1c4ca
unixpunk <steve@hackode.com>
2019-03-14 02:04:38 -0500

    Putting back loop, just in case...

--- plutoweb/overlay/bin/dump1090.sh
+++ plutoweb/overlay/bin/dump1090.sh
@@ -3,4 +3,8 @@
 . /bin/readsettings.sh >/dev/null
 
 cd /dump1090
+while [[ "$autostart" = "dump1090" ]]; do
 ./dump1090 --net --net-http-port 8080 --gain 89 --aggressive
+sleep 1
+. /bin/readsettings.sh >/dev/null
+done
