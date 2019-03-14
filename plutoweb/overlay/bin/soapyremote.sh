791cd4734512dcb13237291158f09a35c73f2b1c
unixpunk <steve@hackode.com>
2019-03-14 01:59:59 -0500

    Cleanup

--- plutoweb/overlay/bin/soapyremote.sh
+++ plutoweb/overlay/bin/soapyremote.sh
@@ -2,10 +2,5 @@
 ## Pull in settings
 . /bin/readsettings.sh >/dev/null
 
-## Start SoapySDRServer
 cd
-while [[ "$autostart" = "SoapyRemote" ]]; do
 /bin/SoapySDRServer --bind
-sleep 1
-. /bin/readsettings.sh >/dev/null
-done
